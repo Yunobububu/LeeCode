36fa1ba310df37a124ce722912b354fdb052d8d9
Yunobububu <lvjinkebit@163.com>
2016-09-16 14:09:55 +0800

    in-place reverse

---
+++ ReverseString_3.c
@@ -0,0 +1,64 @@
+//reverse a string without using extra space
+//in-place revesrse
+#include<stdio.h>
+void main()
+{
+    char str[20];
+    printf("Please input the String\n");
+    gets(str);
+    //Reverse_String2(str);
+    Reverse_String3(str,0,strlen(str));
+    puts(str);
+    return 0;
+}
+
+ Reverse_String(char *p)//without using var
+{
+    char *s,*q;
+    s=p;
+    q=p;
+    while(*p++)
+    {
+        q++;
+    }
+    q-=1;
+    while(q>s)
+    {
+        *q=*s^*q;
+        *s=*q^*s;
+        *q=*s++^*q--;
+    }
+    return s;
+
+}
+char *Reverse_String2(char *p)//use temp var
+{
+    char *s,*q,temp;
+    *s=*p;
+    *q=*p;
+    while(*(p+1)!='\0')
+    {
+        q++;
+        p++;
+    }
+    while(q>s)
+    {
+        *q=temp;
+        *q=*s;
+        *s=temp;
+    }
+    return s;
+}
+Reverse_String3(char *p,left,right)
+{
+    char temp;
+
+    if(left>=right)
+        return;
+
+    s[left]=temp;
+    s[left]=s[right];
+    s[right]=temp;
+    Reverse_String(p,left++,right--);
+
+}
