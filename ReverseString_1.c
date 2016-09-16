b59f26e1985949408afd18dd17e50c1ba5bbd56c
Yunobububu <lvjinkebit@163.com>
2016-09-16 11:59:57 +0800

    print a string in a reverse way

---
+++ ReverseString_1.c
@@ -0,0 +1,22 @@
+//print a string in the reverse way
+#include<stdio.h>
+main()
+{
+    char str[20];
+    char *p;
+    int i=0;
+    gets(str);
+    printf("The original string is:%s\n",str);
+    p=str;
+    while(*p++);
+    p-=2;
+    printf("The reverse  string is:");
+    while(p>=str)
+        {
+            printf("%c",*p);
+            p--;
+        }
+
+
+    return 0;
+}
