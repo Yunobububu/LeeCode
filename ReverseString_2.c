2f7fb5d7091d8c2113a5da7ef0e7d7c31808b31a
Yunobububu <lvjinkebit@163.com>
2016-09-16 10:19:14 +0800

    use point to reverse string

---
+++ ReverseString_2.c
@@ -0,0 +1,20 @@
+//reverse a string
+#include<stdio.h>
+#include<string.h>
+main()
+{
+    char *str[20];
+    char *p;
+    int i=0;
+    gets(str);
+    printf("The original string is:%s\n",str);
+    p=str;
+    i=strlen(str);
+    printf("The reverse  string is:");
+    while(i>0)
+    {
+        printf("%c",*(p+i-1));
+        i--;
+    }
+    return 0;
+}
