1fe308245ceba263efd8247e3d7a7c3b9503f1a6
Mikasa <lvjinkebit@163.com>
2018-05-27 11:48:10 +0800

    Spiral Matrix

--- ImpInJava/SpiralMatrix.java
+++ ImpInJava/SpiralMatrix.java
@@ -1,30 +1,30 @@
+package Leecode.ImpInJava;
+
 import java.util.List;
 import java.util.ArrayList;
 public class SpiralMatrix{
 	private static List<Integer> list = new ArrayList<>(); 
-	public static final int M = 3;
-	public static final int N = 4;
 	public static void main(String[] args){
 		int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 		spiralOrder(matrix);
 		System.out.println(list);
 	}
 	public static List<Integer> spiralOrder(int[][] matrix){
-		if(matrix == null || M < 0 || N < 0){ return null;}
+		if(matrix == null || matrix.length == 0){return list;}
 		int start = 0;
-		int rows = M;
-		int columons = N;
-		while(rows > start * 2 || columons > start * 2){
-			executeFoword(matrix,columons,rows,start);
+		int rows = matrix.length;
+		int columons = matrix[0].length;
+		while(rows > start * 2 && columons > start * 2){
+			execute(matrix,rows,columons,start);
 			start++;
 		}
 		return list;
 	}
-	public static void executeFoword(int[][] matrix,int rows,int columons,int start){
+	public static void execute(int[][] matrix,int rows,int columons,int start){
 		//endX:行的打印终点坐标
-		int endX = rows - start - 1;
+		int endX = columons - start - 1;
 		//endY列的打印终点坐标
-		int endY = columons - start - 1;
+		int endY = rows - start - 1;
 		//从左向右打印 ->
 		for(int i = start; i <= endY;i++){
 			list.add(matrix[start][i]);
